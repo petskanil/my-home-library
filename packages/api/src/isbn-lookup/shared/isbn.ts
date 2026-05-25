40a5604a53f1c7b0746f5103ec95d12cc925aa89
Petter Skau-Nilsen <petskanil@hotmail.com>
2026-05-25 23:18:15 +0200

    Remove unused type

--- packages/api/src/isbn-lookup/shared/isbn.ts
+++ packages/api/src/isbn-lookup/shared/isbn.ts
@@ -5,27 +5,3 @@ export function dedupeIsbns(
     new Set(isbns.filter(Boolean)),
   ) as string[];
 }
-
-export function isValidIsbn13(
-  isbn: string,
-): boolean {
-  if (!/^\d{13}$/.test(isbn)) {
-    return false;
-  }
-
-  let sum = 0;
-
-  for (let i = 0; i < 12; i++) {
-    const digit = Number(isbn[i]);
-
-    sum +=
-      i % 2 === 0
-        ? digit
-        : digit * 3;
-  }
-
-  const checksum =
-    (10 - (sum % 10)) % 10;
-
-  return checksum === Number(isbn[12]);
-}
